5b09fbbb21ed556253b3358bb91788ceebdfcb19
Neillon <neillonwork@gmail.com>
2022-11-30 22:10:22 -0300

    add main js, Formulario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-//
+// FORM entre para o Clube de D&D
+const txtName = document.getElementById('txtName') 
+const txtEmail = document.getElementById('txtEmail') 
+const txtLevel = document.getElementById('txtLevel')
+const txtPersonagem = document.getElementById('txtCharacter') 
+
+const btnSubscribe = document.getElementById('btnSubscribe') 
+
+//Chamada do botao ME inscrever
+btnSubscribe.addEventListener('click', () =>{
+    debugger
+    const subscription = {
+        name: txtName.value,
+        email: txtEmail.value,
+        level: txtLevel.value,
+        personagem: txtPersonagem.value
+    }
+// Salvar objeto no DB...
+}); 
